f8b64106c4e78b95ebfd578c3310d7885a1b33b8
santolp <root@santo-laptop>
2016-06-25 02:27:46 -0300

    se agrego otro texto al abrir.py

--- abrirarchivos.py
+++ abrirarchivos.py
@@ -1,5 +1,5 @@
 arch = open("/home/santo/github/python/holamundo/prueba.txt","a")
-print("Ingrese el texto aqui: ")
+print("Ingrese el texto aqui por favor: ")
 arch.write("\n")
 arch.write(input())
 
